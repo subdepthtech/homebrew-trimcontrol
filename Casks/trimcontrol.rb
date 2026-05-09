ced45c4f05e1b891a1e751af72b72bf669d3ccd9
Austin Tucker <220209011+austinkennethtucker@users.noreply.github.com>
2026-05-08 20:17:14 -0400

    trimcontrol: use versioned cask URL

--- Casks/trimcontrol.rb
+++ Casks/trimcontrol.rb
@@ -2,7 +2,7 @@ cask "trimcontrol" do
   version "0.1.0"
   sha256 "fb8e3a456f9a77b4843326c0bf1cb20533b26d91728eaa4380b65f549d1f823b"
 
-  url "https://github.com/subdepthtech/TrimControl/releases/download/v0.1.0/TrimControl-0.1.0-arm64.dmg"
+  url "https://github.com/subdepthtech/TrimControl/releases/download/v#{version}/TrimControl-#{version}-arm64.dmg"
   name "TrimControl"
   desc "Open source and text files in Neovim from Finder"
   homepage "https://github.com/subdepthtech/TrimControl"
